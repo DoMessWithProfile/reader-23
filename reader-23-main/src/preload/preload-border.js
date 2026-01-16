ed80913ba44aa02d89cb13437f68a4591b3cb0ec
DoMess <michael.lam14@outlook.com>
2026-01-16 16:15:37 +1100

    Delete reader-23-main directory

--- reader-23-main/src/preload/preload-border.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onUpdateHighlight: (callback) => {
-    ipcRenderer.on('update-highlight', (event, rect) => callback(rect));
-  }
-});
